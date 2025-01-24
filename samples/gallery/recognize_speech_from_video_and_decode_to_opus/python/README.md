61b999cc4983dd33babbe2b0a0b4748e9ede1c7a
挪麦 <lengjiayi.ljy@alibaba-inc.com>
2025-01-24 14:59:09 +0800

    [Add] recognize_speech_from_video_and_decode_to_opus in gallery

---
+++ samples/gallery/recognize_speech_from_video_and_decode_to_opus/python/README.md
@@ -0,0 +1,46 @@
+[comment]: # (title and brief introduction of the sample)
+## 视频文件的实时语音识别
+## Python
+
+[comment]: # (prerequisites)
+### :point_right: 前提条件
+
+1. #### 配置阿里云百炼API-KEY
+
+    在运行本示例之前，您需要开通阿里云账号、获得阿里云百炼API_KEY，并进行必要的环境配置。有关API-KEY的详细配置步骤请参考：[PREREQUISITES.md](../../../../PREREQUISITES.md)。
+
+1. #### 安装ffmpeg
+
+    示例需要用到ffmpeg进行音视频解码。推荐从官方网站下载安装，并将ffmpeg安装路径配置进环境变量：[ffmpeg官方网站下载](https://www.ffmpeg.org/download.html)。也可以参考文档[如何安装ffmpeg](../../../docs/QA/ffmpeg.md)。
+
+1. #### 安装Python依赖
+
+    阿里云百炼SDK运行环境需要Python 3.8及以上版本。您可以使用以下命令来安装本示例的依赖：
+    ```commandline
+    pip3 install -r requirements.txt
+    ```
+
+[comment]: # (how to run the sample and expected results)
+### :point_right: 运行示例
+您可以使用以下命令运行本示例：
+
+```commandline
+python3 run.py
+```
+
+示例运行时，本地视频文件会被通过ffmpeg实时转为16k opus音频格式，之后再被实时转写为文字，并通过控制台打印结果。
+
+本示例引入了AudioDecoder类，使用ffmpeg实现了音视频文件的转码，借助于ffmpeg丰富的格式支持，示例支持目前市面上大多数的音视频文件格式转换为适用于语音识别的音频。
+
+### :point_right: 预期结果
+
+完整的识别结果会以json格式保存在```result.json```文件中。完整结果包含句级别和字级别的时间戳信息等。语音识别的纯文本会同时在控制台打印：
+```text
+The brief result is:
+横看成岭侧成峰，远近高低各不同。不识庐山真面目，只缘身在此山中。
+```
+
+[comment]: # (technical support of the sample)
+### :point_right: 技术支持
+<img src="https://dashscope.oss-cn-beijing.aliyuncs.com/samples/audio/group.png" width="400"/>
+
---
+++ samples/gallery/recognize_speech_from_video_and_decode_to_opus/python/run.py
@@ -0,0 +1,96 @@
+# !/usr/bin/env python3
+# Copyright (C) Alibaba Group. All Rights Reserved.
+# MIT License (https://opensource.org/licenses/MIT)
+
+import json
+import os
+import sys
+
+import dashscope
+
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 '../../../utils/python'))
+
+from AudioDecoder import AudioDecoder
+from dashscope.audio.asr import Recognition, RecognitionResult
+
+
+def init_dashscope_api_key():
+    """
+    Set your DashScope API-key. More information:
+    https://github.com/aliyun/alibabacloud-bailian-speech-demo/blob/master/PREREQUISITES.md
+    """
+
+    if 'DASHSCOPE_API_KEY' in os.environ:
+        dashscope.api_key = os.environ[
+            'DASHSCOPE_API_KEY']  # load API-key from environment variable DASHSCOPE_API_KEY
+    else:
+        dashscope.api_key = '<your-dashscope-api-key>'  # set API-key manually
+
+
+class RecognizeSpeechFromSingleFile:
+    def __init__(self):
+        pass
+
+    def recognize_file(self, file_path: str) -> RecognitionResult:
+        # Initialize recognition service by sync call
+        # you can customize the recognition parameters, like model, format, sample_rate
+        # for more information, please refer to https://help.aliyun.com/document_detail/2712536.html
+        recognition = Recognition(
+            model='paraformer-realtime-v2',
+            # 'paraformer-realtime-v1'、'paraformer-realtime-8k-v1'
+            format='opus',
+            # 'pcm'、'wav'、'opus'、'speex'、'aac'、'amr', you can check the supported formats in the document
+            sample_rate=16000,  # supported 8000、16000
+            callback=None)
+
+        # Start recognition with the audio file
+        result = recognition.call(file_path)
+        print(
+            '[Metric] requestId: {}, first package delay ms: {}, last package delay ms: {}'
+            .format(
+                recognition.get_last_request_id(),
+                recognition.get_first_package_delay(),
+                recognition.get_last_package_delay(),
+            ))
+        return result
+
+
+# main function
+if __name__ == '__main__':
+    init_dashscope_api_key()
+
+    # Please replace the path with your audio file path
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, '../../..', 'sample-data',
+                             'sample_video_poetry.mp4')
+    print('Input file is: %s' % file_path)
+
+    decoded_file_path = os.path.join(current_dir, 'temp_decoded.opus')
+
+    # Suggest decode your audio/video file to 16k 16bit mono opus format file to current directory
+    audio_decoder = AudioDecoder()
+    audio_decoder.convert_to_opus_file(file_path, decoded_file_path)
+
+    # Initialize recognition
+    speech_recognizer = RecognizeSpeechFromSingleFile()
+    print('Recognizing speech ...')
+    result = speech_recognizer.recognize_file(decoded_file_path)
+
+    # Check the result
+    file_json = open('result.json', 'wb')
+    file_json.write(
+        json.dumps(result.output, indent=4,
+                   ensure_ascii=False).encode('utf-8'))
+    file_json.close()
+    print('Full recognition result is saved into file: result.json')
+    print('\nThe brief result is:')
+
+    if result.status_code == 200 and result.output:
+        if result.output.__contains__('sentence'):
+            for sent in result.output['sentence']:
+                print(sent['text'], end='')
+
+    # Remove the decoded file
+    os.remove(decoded_file_path)
